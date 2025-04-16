ae799510c33f0ff2aa425c83014aad3608e4af82
akagami <mirkopiras.01@gmail.com>
2025-04-16 19:45:23 +0200

    implemented query function (wip)

--- src/repository.php
+++ src/repository.php
@@ -9,7 +9,16 @@ class Repository {
     }
 
     function query($q) {
-
+        // using mysqli object-oriented
+        $conn = new mysqli($servername, $username, $password);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully to database.";
+        $result = $conn->query($q);
+        if($result->num_rows > 0) {  // so if the query result in a not-empty filtered selection ...
+            return $result; // TO-DO: return the result of the query as a self-made array and not the whole object
+        }
     }
 
 }
