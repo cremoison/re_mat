b5160e7d35642aa35e339d3d46eb32dba4c1f2f0
Cheerios4316 <accfake7214@gmail.com>
2025-04-17 23:43:14 +0200

    added .env

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 require_once __DIR__ . 'vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
 $repo = new Repository($db);
-dump($repo);
+
+
+
